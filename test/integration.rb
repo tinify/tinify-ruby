53f12867f9d2342f8f5107955613f9235fe12275
Rolf Timmermans <rolftimmermans@voormedia.com>
2016-04-01 10:35:30 +0200

    Add integration test for metadata preservation.

--- test/integration.rb
+++ test/integration.rb
@@ -12,27 +12,67 @@ describe "client integration" do
   optimized = Tinify.from_file(unoptimized_path)
 
   it "should compress from file" do
-    Tempfile.open("optimized.png") do |file|
+    Tempfile.open("optimized.png", encoding: "binary") do |file|
       optimized.to_file(file.path)
-      assert_operator file.size, :>, 0
-      assert_operator file.size, :<, 1500
+
+      size = file.size
+      contents = file.read
+
+      assert_operator size, :>, 1000
+      assert_operator size, :<, 1500
+
+      # width == 137
+      assert_includes contents, "\0\0\0\x89".force_encoding("binary")
+      refute_includes contents, "Copyright Voormedia".force_encoding("binary")
     end
   end
 
   it "should compress from url" do
     source = Tinify.from_url("https://raw.githubusercontent.com/tinify/tinify-ruby/master/test/examples/voormedia.png")
-    Tempfile.open("optimized.png") do |file|
+    Tempfile.open("optimized.png", encoding: "binary") do |file|
       source.to_file(file.path)
-      assert_operator file.size, :>, 0
-      assert_operator file.size, :<, 1500
+
+      size = file.size
+      contents = file.read
+
+      assert_operator size, :>, 1000
+      assert_operator size, :<, 1500
+
+      # width == 137
+      assert_includes contents, "\0\0\0\x89".force_encoding("binary")
+      refute_includes contents, "Copyright Voormedia".force_encoding("binary")
     end
   end
 
   it "should resize" do
-    Tempfile.open("resized.png") do |file|
+    Tempfile.open("optimized.png", encoding: "binary") do |file|
       optimized.resize(method: "fit", width: 50, height: 20).to_file(file.path)
-      assert_operator file.size, :>, 0
-      assert_operator file.size, :<, 1000
+
+      size = file.size
+      contents = file.read
+
+      assert_operator size, :>, 500
+      assert_operator size, :<, 1000
+
+      # width == 50
+      assert_includes contents, "\0\0\0\x32".force_encoding("binary")
+      refute_includes contents, "Copyright Voormedia".force_encoding("binary")
+    end
+  end
+
+  it "should preserve metadata" do
+    Tempfile.open("optimized.png", encoding: "binary") do |file|
+      optimized.preserve(:copyright, :creation).to_file(file.path)
+
+      size = file.size
+      contents = file.read
+
+      assert_operator size, :>, 1000
+      assert_operator size, :<, 2000
+
+      # width == 137
+      assert_includes contents, "\0\0\0\x89".force_encoding("binary")
+      assert_includes contents, "Copyright Voormedia".force_encoding("binary")
     end
   end
 end
